f459b2854ae8a3e329c6fc0996741193a5097314
Mauro Diaz <55920871+DuckHappy@users.noreply.github.com>
2024-11-16 23:22:17 -0300

    Add files via upload

---
+++ C/LedPulse.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+#include "esp_timer.h"
+#include "led_strip.h"
+
+//Tag para debugger serie
+static const char *TAG = "Led";
+
+//Define del pin que tiene el led RGB
+#define PIN_RGB 8
+
+//Definimos los colores
+#define ROJO 0
+#define VERDE 1
+#define AZUL 2
+uint8_t color=VERDE;
+uint8_t brillo=0;
+uint8_t sentido=1;
+
+//define del pin del boton
+#define PIN_BOTON 0
+
+//Definimos una variable para manejar el led RGB
+static led_strip_handle_t ledRGB;
+
+//IRQ
+static void IRAM_ATTR rutinaISR_IRQ (void *args)
+{
+    color = (color + 1 ) % 3;
+    gpio_set_level(PIN_BOTON,color);
+}
+
+
+void app_main(void)
+{
+	//Inicializamos la variable global con los datos
+	//del led RGB
+	led_strip_config_t strip_config = {
+        .strip_gpio_num = PIN_RGB,
+        .max_leds = 1, //Solo tenemos un led RGB
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+        .flags.with_dma = false,
+    };
+    //Vamos a llamar a una funcion de ESP-IDF que puede
+    //fallar, asi que usamos una macro que verifica que
+    //todo salga bien.. caso contrario imprime el error
+    //por el debugger serie y llama a abort()
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &ledRGB));
+    //Si llegamos hasta aca, el led  RGB esta configurado correctamente
+    //Lo apagamos
+    led_strip_clear(ledRGB);
+
+    led_strip_set_pixel(ledRGB, 0, 0, 0, 0);
+    led_strip_refresh(ledRGB);
+    ESP_LOGI(TAG,"Comienza...");
+    while(1){
+		vTaskDelay( 10 / portTICK_PERIOD_MS);
+		if (sentido){
+			brillo++;
+			if (brillo==255) sentido=0;
+			} else {
+			brillo--;
+			if (brillo==0){
+				 sentido=1;
+			}
+		}
+		switch(color){
+			case ROJO:
+				led_strip_set_pixel(ledRGB, 0, brillo, 0, 0);
+				break;
+			case VERDE:
+				led_strip_set_pixel(ledRGB, 0, 0, brillo, 0);
+				break;
+			default:
+				led_strip_set_pixel(ledRGB, 0, 0, 0, brillo);
+		}
+		led_strip_refresh(ledRGB);
+
+	}
+
+
+}
---
+++ C/Reloj.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+#include "esp_timer.h"
+#include "tm1637.h"
+#include "led_strip.h"
+
+//Tag para debugger serie
+static const char *TAG = "Reloj";
+
+//Variables globales
+uint8_t segundos=0;
+uint8_t minutos=0;
+
+static void IRAM_ATTR rutinaISR_IRQ (void *args)
+{
+    segundos=-1;
+    minutos=0;
+}
+
+void app_main(void)
+{
+	ESP_LOGI(TAG,"Comienza...");
+	//Definimos el TM1637
+	const gpio_num_t pin_clk = 1;
+	const gpio_num_t pin_dta = 10;
+	tm1637_led_t * lcd = tm1637_init(pin_clk, pin_dta);
+	tm1637_set_segment_number(lcd,0,0x0,1);
+	tm1637_set_segment_number(lcd,1,0x0,1);
+	tm1637_set_segment_number(lcd,2,0x0,1);
+	tm1637_set_segment_number(lcd,3,0x0,1);
+
+	while(1){
+		vTaskDelay( 1000 / portTICK_PERIOD_MS);
+		segundos++;
+		if (segundos==60){
+			segundos=0;
+			minutos++;
+			if (minutos==60){
+				minutos=0;
+			}
+		}
+		uint8_t low = segundos%10;
+		uint8_t high = segundos/10;
+		tm1637_set_segment_number(lcd,3,low,1);
+		tm1637_set_segment_number(lcd,2,high,1);
+		low = minutos%10;
+		high = minutos/10;
+		tm1637_set_segment_number(lcd,1,low,1);
+		tm1637_set_segment_number(lcd,0,high,1);
+	}
+
+}
